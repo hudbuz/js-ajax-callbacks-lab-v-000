dabda43a4d6585a2a38372686885add005ae8b2b
hudbuz <hzb2@cornell.edu>
2016-07-21 17:13:27 -0400

    Done.

--- index.js
+++ index.js
@@ -9,27 +9,33 @@ $(document).ready(function (){
 
 
 
-function searchRepositories() {
-  $(document).ready(function(event){
-  $.get("https://api.github.com/search/repositories?q=" + $('#searchTerms').val(), function(data) {
-    console.log(data)
-    //$.each(data['items'], function(index, repo){
-      //$('#results').html(repo["full_name"])
-      //$('#results').html(repo["description"])
-      //$('#results').html("<a href=" + repo["html_url"] + ">")
-      //$('#results').html(repo["owner"]["login"])
-      //$('#results').html(repo["owner"]["avatar_url"])
-      //$('#results').html("<a href=" + repo["owner"]["html_url"] + ">")
-      //event.preventDefault()
+function searchRepositories(e) {
+  var term = $('#searchTerms').val()
+  var api_url = "https://api.github.com/search/repositories?q="
+  var full_api = api_url + "?utf8=✓&q=" term
 
-    //})
-  })
+  $.get(full_api, function(data) {
+
+    debugger
+    $.each(data['items'], function(index, repo){
+      $('#results').html(repo["full_name"])
+      $('#results').html(repo["description"])
+      $('#results').html("<a href=" + repo["html_url"] + ">")
+      $('#results').html(repo["owner"]["login"])
+      $('#results').html(repo["owner"]["avatar_url"])
+      $('#results').html("<a href=" + repo["owner"]["html_url"] + ">")
+      event.preventDefault()
+
+    })
+  
 })
 }
 
 
 function clickSearch() {
-  $('#search').on('click', searchRepositories )
+  $('#search').on('click', function(event){
+    searchRepositories();
+  } )
 }
 
 function showCommits() {
@@ -37,3 +43,11 @@ function showCommits() {
 
 
 }
+
+
+$(document).ready(function (){
+  
+ searchRepositories()
+ clickSearch()
+  showCommits();
+});
