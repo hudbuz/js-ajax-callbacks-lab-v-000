78fdf1c2a52f7d2b7356e2de6469085e833216e4
hudbuz <hzb2@cornell.edu>
2016-07-21 16:33:38 -0400

    Done.

--- index.js
+++ index.js
@@ -1,8 +1,39 @@
 function handlebarsSetup() {
   //put any handlebars setup in here
-  Handlebars.registerPartial("userDetails", $("#user-details-partial").html())
+  Handlebars.registerPartial("userDetails", $("#results").html())
 }
 
 $(document).ready(function (){
   handlebarsSetup()
 });
+
+
+
+function searchRepositories() {
+  $(document).ready(function(event){
+  $.get("https://api.github.com/search/repositories?q=" + $('#searchTerms').val(), function(data) {
+    console.log(data)
+    //$.each(data['items'], function(index, repo){
+      //$('#results').html(repo["full_name"])
+      //$('#results').html(repo["description"])
+      //$('#results').html("<a href=" + repo["html_url"] + ">")
+      //$('#results').html(repo["owner"]["login"])
+      //$('#results').html(repo["owner"]["avatar_url"])
+      //$('#results').html("<a href=" + repo["owner"]["html_url"] + ">")
+      //event.preventDefault()
+
+    //})
+  })
+})
+}
+
+
+function clickSearch() {
+  $('#search').on('click', searchRepositories )
+}
+
+function showCommits() {
+
+
+
+}
