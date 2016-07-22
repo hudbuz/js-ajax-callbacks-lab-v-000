62ad84fd9bbcf1f820280b48bca38dcd42cb7cb8
hudbuz <hzb2@cornell.edu>
2016-07-22 17:12:50 -0400

    Done.

--- index.js
+++ index.js
@@ -1,53 +1,49 @@
+
+
+function searchRepositories() {
+  const term = $('#searchTerms').val()
+  $.get("https://api.github.com/search/repositories?q=" + term, data => {
+ 
+ const template = Handlebars.compile($('#results').html());
+
+     $('#results').html(template(data))
+   }).fail(displayError())
+
+}
+
+
+
+function displayError() {
+
+  $('#errors').html("Sorry there was an error.");
+
+
+}
+
+function showCommits(el) {
+const owner = el.dataset.owner
+const repo = el.dataset.repository
+ $.get('https://api.github.com/repos/' + owner + '/' + repo + '/commits', data => {
+     const template = Handlebars.compile($('#commits-template').html())
+     $('#details').html(template(data))
+   }).fail(error => {
+     displayError()
+   })
+
+
+}
+
+
+
 function handlebarsSetup() {
   //put any handlebars setup in here
-  Handlebars.registerPartial("userDetails", $("#results").html())
+  Handlebars.registerPartial("userDetails", $("#user-details-partial").html())
 }
 
 $(document).ready(function (){
   handlebarsSetup()
+   
+ 
+  
 });
 
-
-
-function searchRepositories(e) {
-  var term = $('#searchTerms').val()
-  var api_url = "https://api.github.com/search/repositories?q="
-  var full_api = api_url + "?utf8=✓&q=" term
-
-  $.get(full_api, function(data) {
-
-    debugger
-    $.each(data['items'], function(index, repo){
-      $('#results').html(repo["full_name"])
-      $('#results').html(repo["description"])
-      $('#results').html("<a href=" + repo["html_url"] + ">")
-      $('#results').html(repo["owner"]["login"])
-      $('#results').html(repo["owner"]["avatar_url"])
-      $('#results').html("<a href=" + repo["owner"]["html_url"] + ">")
-      event.preventDefault()
-
-    })
-  
-})
-}
-
-
-function clickSearch() {
-  $('#search').on('click', function(event){
-    searchRepositories();
-  } )
-}
-
-function showCommits() {
-
-
-
-}
-
-
-$(document).ready(function (){
-  
- searchRepositories()
- clickSearch()
-  showCommits();
-});
